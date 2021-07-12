d851a732829d6856b3fa2fcfbd86aaeb863b635e
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-07-12 23:49:48 +0600

    Python-Baiscs  exercise done

--- python_basics.py
+++ python_basics.py
@@ -56,13 +56,13 @@ In python, keywords are case sensitive.
 
 Keywords List:
 --------------
-False	await	    else	    import	    pass
-None	break	    except	    in	        raise
-True	class	    finally	    is	        return
-and	    continue	for	        lambda	    try
-as	    def	        from	    nonlocal	while
-assert	del	        global	    not	        with
-async	elif	    if	        or	        yield
+False	   await	      else	    import	   pass
+None	   break	      except	 in	      raise
+True	   class	      finally	 is	      return
+and	   continue	   for	    lambda	   try
+as	      def	      from	    nonlocal	while
+assert	del	      global	 not	      with
+async	   elif	      if	       or	      yield
 """
 
 
